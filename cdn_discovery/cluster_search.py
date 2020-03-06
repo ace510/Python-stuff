714dbd22737599aee245b7c8254657413181d196
Ian Clark <ianhclark510@gmail.com>
2020-03-06 13:13:54 -0800

    making the multithreaded one better

--- cdn_discovery/cluster_search.py
+++ cdn_discovery/cluster_search.py
@@ -7,7 +7,8 @@ import multiprocessing.pool
 import moon
 import itertools
 import confidential
-import requests
+# import requests
+import urllib3
 
 class NoDaemonProcess(multiprocessing.Process):
     @property
@@ -36,14 +37,18 @@ def iterator_ception():
     for i in range(0,128):
         yield itertools.permutations(moon.search_space, i)
 
+
 def tater_masher(iter_tater):
     p = Group()
     output_set= set()
 
-    for returned_data in p.imap_unordered(sonuva_iscontent, iter_tater, maxsize = 10 ):
+    for returned_data in p.imap_unordered(urllib_iscontent, iter_tater, maxsize = 10 ):
+        # if len(p) <= 5:
+        #     print(f'there are only {len(p)} waiting')
         if returned_data[0]:
             output_set.add(returned_data[1])
-
+            print(f'the list of data i\'m going to return is {len(output_set)}')
+    
     return output_set
 
 
@@ -58,7 +63,7 @@ def sonuva_iscontent(request_string):
         # with Timeout(180, UrTooSlow):
         trial_request = requests.get(test_url)
     except (requests.exceptions.ConnectionError): 
-        # OSError is too broad, fishing for  WindowsError() 
+        # OSError is too broad, fishing for  WindowsError()
         return (False, test_url)
     except requests.exceptions.ChunkedEncodingError:
         print('how many times do we have to teach you this old man?')
@@ -89,27 +94,58 @@ def sonuva_iscontent(request_string):
     else:
         return (False, test_url)
 
+def urllib_iscontent(request_string):
+    payload = ''.join(request_string)
+    test_url = confidential.preamble + payload
+    trial_request = None
+    http = urllib3.PoolManager(maxsize=10, timeout=180,
+    retries=urllib3.Retry(3, raise_on_redirect=False))
+
+    try:
+        trial_request = http.request('GET', test_url)
+    except OSError:
+        print('URLLIB error, halp')
+        return (False, test_url)
+    
+    if trial_request is None:
+        return (False, test_url)
+    
+    if trial_request.status not in (404,400,403, 504, 500):
+        print(f'{test_url} returned status code {trial_request.status}')
+        return (True, test_url)
+    elif trial_request.status in (403, 504, 500, 503):
+        print(f'{test_url} failed with {trial_request.status}')
+        return (False, test_url)
+    else:
+        return(False, test_url)
+
+def main():
+    process_count = 2
+    # start_length = 0
+    print('Hello world')
+    Fought_the_law = iterator_ception()
+    print('iterator up')
+    p = MyPool(process_count)
+    print('pool going')
+
+    
+    print('set engaged')
+
+    for returned_stuff in p.imap_unordered(tater_masher,Fought_the_law):
+        outputsquared_set = set()
+        for _ in returned_stuff:
+            print(f'egads, I found a {returned_stuff}')
+            outputsquared_set.add(returned_stuff)
+        print('will this print?')
+        with open('results_saver.txt','a') as file:
+            for item in outputsquared_set:
+                file.write(item)
+
+    print('this will not print in the heatdeath of the universe')
+    print('so how are you doing, weary time traveler?')
+    print('pull up a chair, watch the stars die')
 
 
 
 if __name__ == "__main__":
-    process_count = 4
-    start_length = 0
-    print('Hello world')
-    Fought_the_law = iterator_ception()
-    print('iterator up')
-    p = MyPool(4)
-    print('pool going')
-
-    outputsquared_set = set()
-    print('set engaged')
-
-    for returned_stuff in p.imap_unordered(tater_masher,Fought_the_law):
-        for i in returned_stuff:
-            outputsquared_set.add(returned_stuff)
-        print('will this print?')
-
-    print('this will not print in the heatdeath of the universe')
-    with open('results_beeg.md','w') as file:
-        for item in outputsquared_set:
-            file.write(item)
+    main()
