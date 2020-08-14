1c3bff0984cfce0606b68014d2fec577d8bc5cc6
Ian Clark <ian_h_clark@mac.com>
2020-08-13 21:51:38 -0700

    --- Auto-Git Commit ---

--- Boto/check_pricing.py
+++ Boto/check_pricing.py
@@ -1,2 +1,18 @@
 import boto3
 
+pricing_client = boto3.client('pricing')
+
+service_descriptor : dict = pricing_client.describe_services()
+
+with open('docs/service_discriptors.txt','w') as file:
+    for item in service_descriptor:
+        file.write(str(service_descriptor['Services']))
+
+service_descriptor : dict = pricing_client.describe_services()
+
+for i in service_descriptor['Services']:
+    print(i)
+
+with open('docs/services.txt','w') as file:
+    for item in service_descriptor['Services']:
+        file.write(str(item))
