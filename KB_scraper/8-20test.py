d031f3806f5c0a711ca7e42ba7cbd4eaf17d1dce
Ian Clark <ian_h_clark@mac.com>
2020-08-20 10:52:54 -0700

    --- Auto-Git Commit ---

---
+++ KB_scraper/8-20test.py
@@ -0,0 +1,56 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+import asyncio
+
+
+with open('.token','r') as file:
+    for line in file:
+        QQQ_url = str(line)
+
+foo_dict =''
+
+
+
+human_names = {
+    'BS_SPT_300CRE_SYS_Q_CT'     : '300 CRE',
+    'BS_SPT_300CRE_SYSCCB_Q_CT'  : '300 CRE callbacks',
+    'BS_SPT_100CON_Tech_CCB_Q_CT': '100 CON',
+    'BS_SPT_50C_Install_EN_Q_CT' : '50 Canada',
+    'BS_SPT_50US_ICU_Q_CT'       : '50 US',
+    'BS_SPT_50US_ICU_CCB_Q_CT'   : '50 US Callback',
+}
+
+options = webdriver.ChromeOptions()
+options.add_argument("--log-level=3")
+options.add_argument("--window-size=10,10")
+options.add_argument("--test-type")
+options.add_argument("--disable-extensions")
+options.add_argument("--remote-debugging-port=0")
+options.add_argument("--headless")
+
+
+
+dc = DesiredCapabilities.CHROME
+dc['loggingPrefs'] = {'driver': 'OFF', 'server': 'OFF', 'browser': 'OFF'}
+
+driver = webdriver.Chrome(chrome_options=options, desired_capabilities=dc, executable_path= "C:\\Users\\IaClark\\OneDrive - Sage Software, Inc\\chromedriver_win32\\chromedriver.exe")
+
+def screen_scrape(url):
+    while True:
+        driver.get(url)
+
+        driver_text = driver.find_element_by_tag_name('body').text
+        foo_list = driver_text.split('\n')
+        foo_dict = dict()
+
+        for i in foo_list:
+            stump = i.split(' ')
+            if len(stump) == 12:
+                foo_dict[stump[0]] = stump[1:]
+
+        return foo_dict
