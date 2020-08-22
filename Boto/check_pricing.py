a48735485cbcba415860a143bd8c5f7e529f56da
Ian Clark <ian_h_clark@mac.com>
2020-08-21 20:48:02 -0700

    --- Auto-Git Commit ---

--- Boto/check_pricing.py
+++ Boto/check_pricing.py
@@ -2,6 +2,7 @@ import boto3
 
 def write_file(collection, file_name):
     file_string = str(file_name)
+
     with open(f'docs/{file_string}.txt','w') as file:
         for item in collection:
             file.write(str(item))
@@ -11,19 +12,17 @@ pricing_client = boto3.client('pricing')
 
 service_descriptor : dict = pricing_client.describe_services()
 
-with open('docs/service_discriptors.txt','w') as file:
-    for item in service_descriptor:
-        file.write(str(service_descriptor['Services']))
+# with open('docs/service_discriptors.txt','w') as file:
+#     for item in service_descriptor:
+#         file.write(str(service_descriptor['Services']))
 
-service_descriptor : dict = pricing_client.describe_services()
+write_file(service_descriptor['Services'],'service_discriptor')
 
-with open('docs/services.txt','w') as file:
-    for item in service_descriptor['Services']:
-        file.write(str(item))
 
 ec2_stuff= pricing_client.describe_services(
     ServiceCode='AmazonEC2'
 )
 
-blarga = ec2_stuff['Services'][0]['AttributeNames']
-print(blarga)
+attribute_names = ec2_stuff['Services'][0]['AttributeNames']
+
+write_file(attribute_names,'attribute_names')
