829be2247582cd1d4ac581552f3e7bd6b7dd2eb7
Ian Clark <ian_h_clark@mac.com>
2020-08-23 00:01:49 -0700

    --- Auto-Git Commit ---

--- Boto/get_ec2_price.py
+++ Boto/get_ec2_price.py
@@ -1,22 +1,23 @@
 import boto3
+import json
+from refresh_pricing_docs import write_file
 
 pricing_client = boto3.client('pricing')
 
-boiler_plate = {'Type':'TERM_MATCH'}
+boiler_plate = dict(Type='TERM_MATCH',Field = 'memory',Value = '0.5 GiB')
+filter2 = dict(Type='TERM_MATCH',Field = 'location',Value = 'US East (N. Virginia)')
 
-
-
-boiler_plate['Field'] = 'memory'
-boiler_plate['Value'] = '0.5 GiB'
 
 testy = pricing_client.get_products(
     ServiceCode='AmazonEC2',
-    Filters=[boiler_plate],)
+    Filters=[boiler_plate,filter2
+    ],)
 
 # print(testy['FormatVersion'])
 # testy['PriceList']
 # print(testy['NextToken'])
 
-#print(testy['PriceList'][0])
+test_dict = json.loads(testy['PriceList'][0])
+write_file(test_dict.keys(),'pricelistparts',True)
 
-print(testy['PriceList'][0] is str)
+print(len(testy['PriceList']))
