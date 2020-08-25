78016541fba9b5c87fb8b848bb30bbc1fae00297
Ian Clark <ian_h_clark@mac.com>
2020-08-25 10:48:58 -0700

    --- Auto-Git Commit ---

--- cdn_discovery/guts/8-25-multiprocessing.py
+++ cdn_discovery/guts/8-25-multiprocessing.py
@@ -22,9 +22,21 @@ retries=urllib3.Retry(3, raise_on_redirect=False))
 url_tator = URL_iterator()
 
 task_queue =multiprocessing.Queue(maxsize=PROCS*2)
+result_queue =multiprocessing.Queue(maxsize=PROCS)
+
+process_list = []
+for proc in range(PROCS):
+    p= multiprocessing.Process(target=is_content, args=(task_queue,result_queue, http))
+    p.start()
+    process_list.append(p)
+
 
 while True:
-    while task_queue.qsize < PROCS:
-        task_queue.put([next(url_tator)])
+    while task_queue.qsize() < PROCS:
+        task_queue.put([next(url_tator) for i in range(10000)])
+
+    result_cont  = result_queue.get()
+    for url in result_cont:
+        print(url)
 
 
