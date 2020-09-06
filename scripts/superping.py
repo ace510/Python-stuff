6b8f8ed86a98d0bee674133eec620528e6391d96
Ian Clark <ian_h_clark@mac.com>
2020-09-06 13:27:41 -0700

    --- Auto-Git Commit ---

--- scripts/superping.py
+++ scripts/superping.py
@@ -1,10 +1,12 @@
 import os
+import time
 
 nope = 0
 nope_threshold = 1
 bad_strings = ("Request timed out", " Temporary failure", "could not find host")
 # ping_command ='ping gaqverintapp01.gs.adinternal.com'
-ping_command = "ping www.google.com"
+# ping_command = "ping www.google.com"
+ping_command = 'ping www.myphysicslab.com'
 while True:
     up = True
     print("pinging")
@@ -14,22 +16,23 @@ while True:
         if b_str in output:
             print(f"found: {b_str}")
             up = False
+            time.sleep(1)
 
     if up == True:
         print("site seems to be up")
         print(output)
         break
 
-up = False
-nope = 0
-nope_threshold = 1
-# ping_command ='ping gaqverintapp01.gs.adinternal.com'
-ping_command = "ping trueye.dyndns.org"
-while up is False:
-    output = os.popen(ping_command).read()
-    print(output)
-    if "Request timed out" not in output:
-        up = True
-        print(output)
-    else:
-        nope += 1
+# up = False
+# nope = 0
+# nope_threshold = 1
+# # ping_command ='ping gaqverintapp01.gs.adinternal.com'
+# ping_command = "ping trueye.dyndns.org"
+# while up is False:
+#     output = os.popen(ping_command).read()
+#     print(output)
+#     if "Request timed out" not in output:
+#         up = True
+#         print(output)
+#     else:
+#         nope += 1
