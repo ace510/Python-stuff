bfb75595a3c2f8235fb891334dbc22d1ca51120e
Ian Clark <ian_h_clark@mac.com>
2020-09-07 14:02:37 -0700

    --- Auto-Git Commit ---

---
+++ scripts/this_website_will_self.py
@@ -0,0 +1,34 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+payload = 'https://www.thiswebsitewillselfdestruct.com/'
+
+options = webdriver.ChromeOptions()
+options.add_argument("--log-level=3")
+options.add_argument("--window-size=10,10")
+options.add_argument("--test-type")
+options.add_argument("--remote-debugging-port=0")
+options.add_argument("--headless")
+
+dc = DesiredCapabilities.CHROME
+dc["loggingPrefs"] = {"driver": "OFF", "server": "OFF", "browser": "OFF"}
+
+driver = webdriver.Chrome(
+    chrome_options=options,
+    desired_capabilities=dc,
+    executable_path=r"D:\\chromedriver\\chromedriver.exe",
+)
+
+driver.get(payload)
+# time.sleep(15)
+innerHTML = driver.execute_script("return document.body.innerHTML")
+
+print(innerHTML.count('<h3>'))
+
+'<h3>(in 82,024 seconds)</h3>'
+
